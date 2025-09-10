47a1eb917a5bd73e227612137e16ac79b374ca11
Patricia Sánchez Nadales <patriciasancheznadales95@gmail.com>
2025-09-10 10:46:41 +0200

    End signals

--- angular/signals-lab/src/app/reader.component.ts
+++ angular/signals-lab/src/app/reader.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, input, Input, signal } from '@angular/core';
 
+function quoteString(value: string | undefined) {
+  if (value) {
+    return `"${value}"`;
+  }
+  return '';
+}
 @Component({
   selector: 'app-reader',
   imports: [],
@@ -7,9 +13,12 @@ import { Component, Input, signal } from '@angular/core';
   styles: ``,
 })
 export class ReaderComponent {
-  inputSignal = signal<string>('');
-
-  @Input() set input(value: string) {
-    this.inputSignal.set(value);
-  }
+  // inputSignal = input<string>('');
+  //You can also use required if you want to ensure the input is provided
+  // you can also use alias to have a different name for the input property
+  // or use transform to transform the input value before setting the signal
+  inputSignal = input.required<string, string>({
+    transform: quoteString,
+    alias: 'input',
+  });
 }
